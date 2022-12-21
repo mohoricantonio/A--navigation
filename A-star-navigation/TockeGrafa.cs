f682eb318e9b5ccabf15eab53527c755b54a0c47
Antonio Mohorić <mohoricantonio@gmail.com>
2022-12-21 22:05:06 +0100

    Class created
    TockeGrafa.cs class created

---
+++ A-star-navigation/TockeGrafa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_star_navigation
+{
+    public class TockeGrafa
+    {
+        public double lon;
+        public double lat;
+        public List<TockeGrafa> ListaSusjeda;
+        public TockeGrafa(double lon, double lat)
+        {
+            this.lon = lon;
+            this.lat = lat;
+            ListaSusjeda = new List<TockeGrafa>();
+        }
+        
+    }
+}
