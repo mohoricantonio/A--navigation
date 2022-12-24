8b29b4c11b1e2663790196bd1c48fe12b3a94289
Antonio Mohorić <mohoricantonio@gmail.com>
2022-12-24 11:59:16 +0100

    Removed route calculation

--- A-star-navigation/AStarCalculator.cs
+++ A-star-navigation/AStarCalculator.cs
@@ -24,58 +24,22 @@ namespace A_star_navigation
             List<TockaGrafa> otvorena = new List<TockaGrafa>();
             List<TockaGrafa> zatvorena = new List<TockaGrafa>();
 
-            prethodnaUdaljenost[pocetnaTocka] = 0;
             TockaGrafa trenutna = pocetnaTocka;
             TockaGrafa prethodna = pocetnaTocka;
-            tezinaTocke[trenutna] = VratiUdaljenost(trenutna, zavrsnaTocka) + prethodnaUdaljenost[trenutna];
 
-            while (trenutna != zavrsnaTocka)
+            /*while (trenutna != zavrsnaTocka)
             {
-                zatvorena.Add(trenutna);
+                --TO DO-- 
+            }*/
+            // dodaj zadnju točku u listu zatvorena!
 
-                foreach (TockaGrafa t in trenutna.ListaSusjeda)
-                {
-                    if (prethodna == t) continue;
-                    if (tezinaTocke.ContainsKey(t) == true)
-                    {
-                        if (tezinaTocke[t] > (prethodnaUdaljenost[trenutna] + VratiUdaljenost(t, trenutna)+ 
-                            VratiUdaljenost(t, zavrsnaTocka)))
-                        {
-                            prethodnaUdaljenost[t] = prethodnaUdaljenost[trenutna] + VratiUdaljenost(t, trenutna);
-                            tezinaTocke[t] = prethodnaUdaljenost[t] + VratiUdaljenost(t, zavrsnaTocka);
-                            otvorena.Add(t);
-                        }
-                    }
-                    else
-                    {
-                        prethodnaUdaljenost[t] = prethodnaUdaljenost[trenutna] + VratiUdaljenost(t, trenutna);
-                        tezinaTocke[t] = prethodnaUdaljenost[t] + VratiUdaljenost(t, zavrsnaTocka);
-                        otvorena.Add(t);
-                    }
-                    
-                }
-                prethodna = trenutna;
-                trenutna = VratiTockuNajmanjeTezine(otvorena, tezinaTocke);
-                otvorena.Remove(trenutna);
-            }
-            zatvorena.Add(trenutna);
-
-            return VratiKrajnjuRutu(zatvorena);
+            //return VratiKrajnjuRutu(zatvorena);
+            return "TODO";
         }
         private static TockaGrafa VratiTockuNajmanjeTezine(List<TockaGrafa> lista, Dictionary<TockaGrafa, double> tezinaTocke)
         {
             TockaGrafa returnMe = null;
-            for(int i = lista.Count-1; i>0; i--)
-            {
-                if(returnMe == null)
-                {
-                    returnMe = lista[i];
-                }
-                for(int j = 0; j<i; j++)
-                {
-                    if (tezinaTocke[lista[j]] < tezinaTocke[returnMe]) returnMe = lista[j];
-                }
-            }
+            // --TO DO--
             return returnMe;
         }
         private static string VratiKrajnjuRutu(List<TockaGrafa> lista)
@@ -96,12 +60,10 @@ namespace A_star_navigation
                     }
                 }
             }
-            
             for (int i = zavrsnaLista.Count-1; i>= 0; i--)
             {
                 returnMe += zavrsnaLista[i].naziv + "->";
             }
-            
             return returnMe.Substring(0, returnMe.Length -2);
         }
     }
